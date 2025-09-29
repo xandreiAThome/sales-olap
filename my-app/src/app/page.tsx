cbc81c394ee8009fc0a56c770ad620a671f23514
chiwasushuba <joshua11084@gmail.com>
2025-09-29 22:25:04 +0800

    init frontend

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Web App STADVDB</h1>
+    </div>
+  );
+}
