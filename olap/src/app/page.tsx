c8c0728895fce863e8a39452bab9468e223b0afb
whoops <142461198+chiwasushuba@users.noreply.github.com>
2025-09-30 09:17:10 +0800

    Feature/olap init (#9)
    * init frontend
    
    * fix: removed extra my-app
    
    * feat: added test graphs using chart.js

---
+++ olap/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center h-screen">
+      <h1 className="text-3xl font-bold underline">
+        Online Analytical Processing (OLAP) Reports{" "}
+      </h1>
+    </div>
+  );
+}
